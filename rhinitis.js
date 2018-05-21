bd02f3f547fe0a57cf435a949bb3cf535f24cff4
HAOYUatHZ <haoyu@protonmail.com>
2018-05-21 16:19:36 +0800

    promise

--- rhinitis.js
+++ rhinitis.js
@@ -2,12 +2,33 @@ var request = require('request');
 
 
 
-request.post(
-    'http://127.0.0.1:8545/',
-    { json: {"method": "personal_listAccounts", "params": [], "id":1} },
-    function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body)
+// request.post(
+//     'http://127.0.0.1:8545/',
+//     { json: {"method": "personal_listAccounts", "params": [], "id":1} },
+//     function (error, response, body) {
+//         if (!error && response.statusCode == 200) {
+//             console.log(body)
+//         }
+//     }
+// );
+
+var promise = new Promise(function(resolve, reject) {
+    request.post(
+        'http://127.0.0.1:8545/',
+        { json: {"method": "personal_listAccounts", "params": [], "id":1} },
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                resolve(body);
+            }
+            else {
+                reject(Error(error));
+            }
         }
-    }
-);
+    )
+});
+
+promise.then(function(result) {
+  console.log(result);
+}, function(err) {
+  console.log(err);
+});
